861adcba3892e7a7849f6dbb84fa7825b5777d7a
Slavat <tymosh@gmail.com>
2020-12-28 12:15:33 +0100

    Changes

--- src/js/myCode.js
+++ src/js/myCode.js
@@ -1,41 +1,51 @@
-const refs = {
-  body: document.querySelectorAll('body'),
-  days: document.querySelector("span[data-value='days']"),
-  hours: document.querySelector("span[data-value='hours']"),
-  minutes: document.querySelector("span[data-value='mins']"),
-  seconds: document.querySelector("span[data-value='secs']"),
-};
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.targetDate = targetDate;
+    this.timer = document.getElementById(selector);
+    this.days = this.timer.querySelector('span[data-value="days"]');
+    this.hours = this.timer.querySelector('span[data-value="hours"]');
+    this.mins = this.timer.querySelector('span[data-value="mins"]');
+    this.secs = this.timer.querySelector('span[data-value="secs"]');
+  }
 
-function count() {
-  setInterval(() => {
-    const targetDate = new Date('Dec 20, 2020');
-    let time = targetDate - Date.now();
-    const times = getTimeComponents(time);
-    updateClock(times);
-  }, 1000);
+  start() {
+    this.updateTimerOutput();
+    setInterval(() => {
+      this.updateTimerOutput();
+    }, 1000);
+  }
+
+  updateTimerOutput() {
+    let time = this.targetDate - Date.now();
+    const { days, hours, mins, secs } = this.calculateTime(time);
+    this.days.textContent = days;
+    this.hours.textContent = hours;
+    this.mins.textContent = mins;
+    this.secs.textContent = secs;
+  }
+
+  calculateTime(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 }
 
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
+const countNewYear = new CountdownTimer({
+  selector: 'timer-1',
+  targetDate: new Date('Jan 1, 2021'),
+});
 
-function getTimeComponents(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  return { days, hours, mins, secs };
-}
-
-function updateClock({ days, hours, mins, secs }) {
-  refs.days.textContent = days;
-  refs.hours.textContent = hours;
-  refs.minutes.textContent = mins;
-  refs.seconds.textContent = secs;
-}
-count();
+countNewYear.start();
 
 
 /*
